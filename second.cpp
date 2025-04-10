3eef69ef1e314ce469c705016d422c79bc614131
RishiAgarwal3 <rishiagarwal173@gmail.com>
2025-04-10 13:26:23 +0530

    Update second.cpp

--- second.cpp
+++ second.cpp
@@ -1,6 +1,19 @@
-#include <iosteam.h>
-#include <math.h>
+// variables
 
-void main()
-{
-cout<< "hello world";
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int x = 5;
+    int y = 23;
+
+    int sum = x+y;
+
+    cout<<"the sum of x and y is : \n"<<sum;
+
+    return 0;
+
+
+}
